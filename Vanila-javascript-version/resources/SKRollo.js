57c0bbb99cce42b9fdfce595d01f35c68ed9f7ac
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-02-26 09:37:43 +0200

    initialization of parameters

--- Vanila-javascript-version/resources/SKRollo.js
+++ Vanila-javascript-version/resources/SKRollo.js
@@ -1,8 +1,44 @@
 //! * Authored by SKindij. * MIT Licence *
 //! This project is my personal idea and development, which I do as much as possible in my free time.
 //! I will be glad and grateful to everyone for advice on its improvement.
-
 import './SKRollo.css';
+
+//* initialization of parameters 
+const openingWidth = document.querySelector("#openingWidth");
+const openingHeight = document.querySelector("#openingHeight");
+  const profileType = document.querySelector("#profileType");
+  const controlType = document.querySelector("#controlType");
+  const instalType = document.querySelector("#instalType");
+const beginCalc = document.querySelector("#beginCalc");
+const Section2 = document.querySelector("#Section2");
+  const roletColor = document.querySelector("#roletColor");
+  const hangers = document.querySelector("#hangers");
+  const tubeType = document.querySelector("#tubeType");
+  const boxType = document.querySelector("#boxType");
+  const guideRails = document.querySelector("#guideRails");
+const continueCalc = document.querySelector("#continueCalc");
+const Section3 = document.querySelector("#Section3");
+let rolloWidth = document.querySelector("#rolloWidth");
+let rolloHeight = document.querySelector("#rolloHeight");
+  const lockingType = document.querySelector("#lockingType");
+const finishCalc = document.querySelector("#finishCalc");
+  const orderNumber = document.querySelector("#orderNumber");
+const downloadCalc = document.querySelector("#downloadCalc");
+const Section4 = document.querySelector("#Section4");
+
+Section2.style.visibility = "hidden";
+Section3.style.visibility = "hidden";
+downloadCalc.style.visibility = "hidden";
+Section4.style.visibility = "hidden";
+
+
+
+
+
+
+
+
+
 
 
 
