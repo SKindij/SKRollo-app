b985ce46d5d098eeacde241aabd4f494e489b90f
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-02-26 14:40:14 +0200

    production list
    forming a complete list of components for production

--- Vanila-javascript-version/resources/SKRollo.js
+++ Vanila-javascript-version/resources/SKRollo.js
@@ -443,7 +443,66 @@ function continueCalculation() {
 }
 continueCalc.addEventListener('click', continueCalculation);
 
+//* forming a complete list of components for production
+let orderInfo = document.querySelector("#orderInfo");
+const measPosArticles = document.querySelector("#measPosArticles");
+  let protBoxArt = measPosArticles.querySelector("#protBoxArt");  // 1
+  let octaShaftArt = measPosArticles.querySelector("#octaShaftArt"); // 2
+  let profileArt = measPosArticles.querySelector("#profileArt"); // 3
+  let connectArt = measPosArticles.querySelector("#connectArt"); // 4
+  let endBarArt = measPosArticles.querySelector("#endBarArt"); // 5
+  let laneArt = measPosArticles.querySelector("#laneArt"); // 6
+  let lowRubberArt = measPosArticles.querySelector("#lowRubberArt"); // 7
+  let guideRailsArt = measPosArticles.querySelector("#guideRailsArt"); // 8
+  let brushInsertArt = measPosArticles.querySelector("#brushInsertArt"); // 9
 
+const measPosLength = document.querySelector("#measPosLength");
+  let protBoxLength = measPosLength.querySelector("#protBoxLength");  // 1
+  let octaShaftLength = measPosLength.querySelector("#octaShaftLength"); // 2
+  let profileLength = measPosLength.querySelector("#profileLength"); // 3
+  let connectLength = measPosLength.querySelector("#connectLength"); // 4
+  let endBarLength = measPosLength.querySelector("#endBarLength"); // 5
+  let laneLength = measPosLength.querySelector("#laneLength"); // 6
+  let lowRubberLength = measPosLength.querySelector("#lowRubberLength"); // 7
+  let guideRailsLength = measPosLength.querySelector("#guideRailsLength"); // 8
+  let brushInsertLength = measPosLength.querySelector("#brushInsertLength"); // 9
+
+const measPosQuanty = document.querySelector("#measPosQuanty");
+  let protBoxQuant = measPosQuanty.querySelector("#protBoxQuant");  // 1
+  let octaShaftQuant = measPosQuanty.querySelector("#octaShaftQuant"); // 2
+  let profileQuant = measPosQuanty.querySelector("#profileQuant"); // 3
+  let connectQuant = measPosQuanty.querySelector("#connectQuant"); // 4
+  let endBarQuant = measPosQuanty.querySelector("#endBarQuant"); // 5
+  let laneQuant = measPosQuanty.querySelector("#laneQuant"); // 6
+  let lowRubberQuant = measPosQuanty.querySelector("#lowRubberQuant"); // 7
+  let guideRailsQuant = measPosQuanty.querySelector("#guideRailsQuant"); // 8
+  let brushInsertQuant = measPosQuanty.querySelector("#brushInsertQuant"); // 9
+
+const PosArticles = document.querySelector("#PosArticles");
+  let sideCoversArt = PosArticles.querySelector("#sideCoversArt");  // 1
+  let ringsShaftArt = PosArticles.querySelector("#ringsShaftArt"); // 2
+  let hangersArt = PosArticles.querySelector("#hangersArt"); // 3
+  let capsuleArt = PosArticles.querySelector("#capsuleArt"); // 4
+  let bearingArt = PosArticles.querySelector("#bearingArt"); // 5
+  let bearBracketArt = PosArticles.querySelector("#bearBracketArt"); // 6
+  let springArt = PosArticles.querySelector("#springArt"); // 7
+  let plateArt = PosArticles.querySelector("#plateArt"); // 8
+  let boltLockArt = PosArticles.querySelector("#boltLockArt"); // 9
+  let stopperArt = PosArticles.querySelector("#stopperArt"); // 10
+  let plugsArt = PosArticles.querySelector("#plugsArt"); // 11
+
+  const PosQuanty = document.querySelector("#PosQuanty");
+  let sideCoverQuant = PosQuanty.querySelector("#sideCoverQuant");  // 1
+  let ringsShafQuant = PosQuanty.querySelector("#ringsShafQuant"); // 2
+  let hangersQuant = PosQuanty.querySelector("#hangersQuant"); // 3
+  let capsuleQuant = PosQuanty.querySelector("#capsuleQuant"); // 4
+  let bearingQuant = PosQuanty.querySelector("#bearingQuant"); // 5
+  let bearBracketQuant = PosQuanty.querySelector("#bearBracketQuant"); // 6
+  let springQuant = PosQuanty.querySelector("#springQuant"); // 7
+  let plateQuant = PosQuanty.querySelector("#plateQuant"); // 8
+  let boltLockQuant = PosQuanty.querySelector("#boltLockQuant"); // 9
+  let stopperQuant = PosQuanty.querySelector("#stopperQuant"); // 10
+  let plugsQuant = PosQuanty.querySelector("#plugsQuant"); // 11
 
 
 
