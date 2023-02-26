a271e21072dec0f0d9dbc04f204bed2587058bdf
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-02-26 14:38:24 +0200

    SECOND FUNCTIONAL BUTTON
    function continueCalculation()

--- Vanila-javascript-version/resources/SKRollo.js
+++ Vanila-javascript-version/resources/SKRollo.js
@@ -409,6 +409,55 @@ function setSmallDetails() {
   plugsArt.textContent = 'PP12 or PP14' + roletColor.value;
 } 
 
+//** SECOND FUNCTIONAL BUTTON
+function continueCalculation() {
+  try {
+    let initialParameters = [roletColor.value, hangers.value, tubeType.value, boxType.value, lockingType.value];
+    initialParameters.forEach((item) => {
+      if (item === '') { 
+        throw new Error('Option is not selected') 
+        } 
+    });
+    console.log('continuation of calculation');
+    //** call the functions
+    setRolletsDimensions();
+    console.log('roller shutter color:', roletColor.value);
+      profileArt.textContent = profileType.value + '-' + roletColor.value;
+    console.log('box size:', boxType.value);
+      protBoxArt.textContent = boxType.value + '-' + roletColor.value;
+      setSideCoverType();
+    console.log('shaft diameter:', tubeType.value);
+      octaShaftArt.textContent = tubeType.value;
+      calculateCanvasWeight();
+      setSmallDetails();
+    console.log('guide rails:', guideRails.value);
+      guideRailsArt.textContent = guideRails.value + '-' + roletColor.value;
+    console.log('locking:', lockingType.value);
+      setEndBarType();
+      setTimeout(() => {Section3.style.visibility = "visible";}, 500);
+  } catch (Error) {
+    console.log(Error); // Option is not selected
+    alert('Not all options are selected');
+    return false;
+  }
+}
+continueCalc.addEventListener('click', continueCalculation);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
