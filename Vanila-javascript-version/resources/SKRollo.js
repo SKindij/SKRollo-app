b0e08857170f988a79e501505370e17df1b61df1
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-02-26 09:35:40 +0200

    Create SKRollo.js

---
+++ Vanila-javascript-version/resources/SKRollo.js
@@ -0,0 +1,10 @@
+//! * Authored by SKindij. * MIT Licence *
+//! This project is my personal idea and development, which I do as much as possible in my free time.
+//! I will be glad and grateful to everyone for advice on its improvement.
+
+import './SKRollo.css';
+
+
+
+
+
